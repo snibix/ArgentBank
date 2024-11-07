a45cb3c549bd82f53756ffc0eb6949b078a6f77c
Damien <damien.dev2022@gmail.com>
2024-11-07 18:08:02 +0100

    refactoring page User

--- front/src/components/UserHeader.jsx
+++ front/src/components/UserHeader.jsx
@@ -1,15 +1,17 @@
+import Button from "./Button";
+
 function UserHeader() {
   return (
-    <>
-      <div className="header">
-        <h1>
-          Welcome back
-          <br />
-          Tony Jarvis!
-        </h1>
-        <button className="edit-button">Edit Name</button>
-      </div>
-    </>
+    <div className="header">
+      <h1>
+        Welcome back
+        <br />
+        Tony Jarvis!
+      </h1>
+      <Button className="edit-button" type="button">
+        Edit Name
+      </Button>
+    </div>
   );
 }
 
