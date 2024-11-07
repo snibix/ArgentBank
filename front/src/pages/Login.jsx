c8ebe91bb219536399ac0caf2c3291e26387ffd0
Damien <damien.dev2022@gmail.com>
2024-11-07 14:27:57 +0100

    preparation des pages

---
+++ front/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Login() {
+  return <div>Login</div>;
+}
+
+Login.propTypes = {};
+
+export default Login;
---
+++ front/src/pages/User.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function User(props) {
+  return <div>User</div>;
+}
+
+User.propTypes = {};
+
+export default User;
