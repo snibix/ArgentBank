2d6f5694c11d164f295d922dabde7ac5b663427c
Damien <damien.dev2022@gmail.com>
2024-11-08 10:51:29 +0100

    installation de redux

---
+++ front/redux/slices/authSlice.js
@@ -0,0 +1,57 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const login = createAsyncThunk(
+  "auth/login",
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const response = await fetch("http://localhost:3001/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Connexion échouée");
+      }
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+const authSLice = createSlice({
+  name: "auth",
+  initialState: {
+    user: null,
+    token: null,
+    status: "idle",
+    error: null,
+  },
+  reducers: {
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.pending, (state) => {
+        state.status = "loading";
+        state.error = null;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      });
+  },
+});
+
+export const { logout } = authSLice.actions;
+export default authSLice.reducer;
